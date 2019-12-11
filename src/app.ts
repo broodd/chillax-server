646c7bb01b713fc3d6dbb00b651bc3e620adca9f
Broodd <svyatddev@gmail.com>
2019-12-11 11:45:46 +0200

    remove trash

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,7 @@ import lusca from 'lusca';
 import compression from 'compression';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
-// import flash from 'express-flash';
-import path from 'path';
 import logger from './util/logger';
-import asyncWrapper from './util/error-handler';
 import { MONGODB_URI } from './util/secrets';
 
 /**
@@ -47,29 +44,6 @@ app.use(lusca.xframe('SAMEORIGIN'));
 app.use(lusca.xssProtection(true));
 app.use('/static', express.static('static'));
 
-/**
- * maybe need it later
- */
-// app.use((req, res, next) => {
-//   res.locals.user = req.user;
-//   next();
-// });
-// app.use((req, res, next) => {
-//   // After successful login, redirect back to the intended page
-//   if (!req.user &&
-//     req.path !== '/login' &&
-//     req.path !== '/signup' &&
-//     !req.path.match(/^\/auth/) &&
-//     !req.path.match(/\./)) {
-//     req.session.returnTo = req.path;
-//   } else if (req.user &&
-//     req.path == '/account') {
-//     req.session.returnTo = req.path;
-//   }
-//   next();
-// });
-
-
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
@@ -80,6 +54,9 @@ app.use((req, res, next) => {
   next();
 });
 
+/**
+ * Uploading files
+ */
 import multer from './middlewares/upload';
 app.use(multer);
 
--- src/cluster.ts
+++ src/cluster.ts
@@ -1,17 +1,19 @@
 import cluster from 'cluster';
 import server from './server';
+import logger from './util/logger';
 
 if (cluster.isMaster) {
   const cpuLength = require('os').cpus().length;
 
-  for (let i = 0; i < cpuLength - 1; i += 1) {
+  logger.debug(`App running on ${cpuLength} processes`);
+  for (let i = 0; i < cpuLength; i += 1) {
     cluster.fork();
   }
+
+  cluster.on('exit', () => {
+    // Replace the dead worker,
+    cluster.fork();
+  });
 } else {
   server();
 }
-
-cluster.on('exit', () => {
-  // Replace the dead worker,
-  cluster.fork();
-});
